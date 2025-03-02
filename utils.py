cf94f7d362e9124b311b5d16ce0bae9de0e3bca5
alessandroardenghi <alessandro.ardenghi.aa@gmail.com>
2025-03-02 22:18:18 +0100

    modified UCB function and modified is_win function

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def is_win(grid, winning_configurations):
             if (player0 & config) == config:
                 return 1
             if (player1 & config) == config:
-                return 1
+                return 2
         return 0
 
 def is_full(grid, size):
@@ -65,11 +65,38 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def compute_ucb(parent, V , N, parent_N):
-    if parent is None:
+def compute_ucb(leaf):
+    if leaf.parent is None:
         print('CANNOT COMPUTE UCB FOR THE ROOT')
         return None
-    if N == 0:
+    if leaf.N == 0:
         return math.inf
-    return V/N + 2* math.sqrt((2 * math.log(parent_N)) / N)
+    return leaf.V/leaf.N + 2* math.sqrt((2 * math.log(leaf.parent.N)) /leaf.N)
 
+def display_board(grid, size):
+
+    bin0 = np.array(list(bin(grid[0])[2:].zfill(size**2)[::-1]), dtype=int)
+    bin1 = np.array(list(bin(grid[1])[2:].zfill(size**2)[::-1]), dtype=int)
+
+    board_grid = (bin0 - bin1).reshape(size, size)  
+
+    for i, row in enumerate(board_grid):
+
+        row_display = " | ".join('X' if cell == 1 else 'O' if cell == -1 else ' ' for cell in row)
+        
+        print(" " + row_display + " ")
+
+        if i < len(board_grid) - 1:
+            print("---+" * (size - 1) + "---")
+            
+            
+def is_move_forced(grid, winning_configurations):
+    conf1, conf2 = grid
+    for configuration in winning_configurations:
+        if bin(configuration & conf1).count('1') == 2 and bin(configuration & conf2).count('1') == 0:
+            #print(configuration & ~conf1)
+            return int(math.log2(configuration & ~conf1))
+        if bin(configuration & conf2).count('1') == 2 and bin(configuration & conf1).count('1') == 0:
+            #print(configuration & ~conf2)
+            return int(math.log2(configuration & ~conf2))
+    return None
